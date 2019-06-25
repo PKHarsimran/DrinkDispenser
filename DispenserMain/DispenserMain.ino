e2583cc36b966f0c1ffa67422bf38224c1fdf1b1
Harsimran sidhu <harsimransidhu770@gmail.com>
2019-06-24 21:45:11 -0300

    Add files via upload

--- DispenserMain/DispenserMain.ino
+++ DispenserMain/DispenserMain.ino
@@ -1,12 +1,22 @@
+#include <ClickEncoder.h>
+
 /*
  * Displays text sent over the serial port (e.g. from the Serial Monitor) on
  * an attached LCD.
  */
+#include <Encoder.h>
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 
 #define BACKLIGHT_PIN     13
-
+int val;
+int encoder0PinA = 11;
+int encoder0PinB = 12;
+int encoder0Pos = 0;
+int encoder0PinALast = LOW;
+int n = LOW;
+#define enCoder0Btn 12
+int enCoderPos = 0;
 int R1 = 2;
 int R2 = 3;
 int R3 = 4;
@@ -18,7 +28,9 @@ int R8 = 9;
 char Data = 0;
   int selection = 0;
    char *ComboArray[] = {"Rum & Coke", "Gin & Tonic", "Long Island", "Scredriver","Margarita","Gin & Juice","Tequila Sunrise"};
-//LiquidCrystal_I2C lcd(0x27);  // set the LCD address to 0x38
+   boolean Dispense = false;
+   unsigned long previousTimer  = 0;
+   //LiquidCrystal_I2C lcd(0x27);  // set the LCD address to 0x38
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  
 void setup()
 {
@@ -34,11 +46,11 @@ void setup()
               pinMode(R7,OUTPUT);
                 pinMode(R8,OUTPUT);
                 
- 
+  pinMode (encoder0PinA, INPUT);
+  pinMode (encoder0PinB, INPUT);
   Serial.begin(9600);
    lcd.setCursor(0,0);
    lcd.write("Welcome :)");
-   
    // Keep the Relays to off position !
    digitalWrite(R1, HIGH);
       digitalWrite(R2, HIGH);
@@ -52,15 +64,31 @@ void setup()
                         
  
 }
-
+long oldPosition  = -999;
 void loop()
 {
+
+   enCoder();
+  
 SerialRead();
 
- if (Data == 'D')
-     DrinkSelected();
+ if (Dispense)
+   DrinkSelected();
 }
-
+void enCoder()
+{
+   n = digitalRead(encoder0PinA);
+  if ((encoder0PinALast == LOW) && (n == HIGH)) {
+    if (digitalRead(encoder0PinB) == LOW) {
+      encoder0Pos--;
+    } else {
+      encoder0Pos++;
+    }
+  }
+  encoder0PinALast = n;
+  selection = encoder0Pos;
+  CombosDisplay(selection);
+}
 void SerialRead(){
   if (Serial.available()>0) 
   {
@@ -72,8 +100,10 @@ void SerialRead(){
      Serial.println(Data);
       //DebuggerPrint(Data); 
      ConditionFun(Data);  
-     if (Data == 'D')
+     if (Data == 'D'){
      DrinkSelected();
+     Dispense = true;
+     }
  }
 
 }
@@ -95,6 +125,7 @@ void ConditionFun(char Data){
 void CombosDisplay(int s) {
  // DebuggerPrint(String(sizeof(ComboArray)));
   int olds = s;
+  
     lcd.setCursor(0,0);
     lcd.clear();
     switch (s) {
@@ -131,79 +162,92 @@ void DebuggerPrint(String D)
 }
 void DrinkSelected()
 {
+   
     switch (selection) {
      case 1 :
      // Rum & Coke
-      DrinkTimer(50000,0,0,0,0,150000,0,0);
+      DrinkTimer(50000,0,0,0,0,15000,0,0);
       break;
       case 2:
       // Gin & Tonic
-      DrinkTimer(0,50000,0,0,0,0,150000,0);
+      DrinkTimer(0,50000,0,0,0,0,15000,0);
       break;
       case 3 :
       // Long Island
-       DrinkTimer(15000,15000,15000,15000,0,100000,0,150000);
+       DrinkTimer(15000,15000,15000,15000,0,10000,0,15000);
       break;
       case 4 :
-    DrinkTimer(0,0,50000,0,0,0,0,150000);
+      //Scewdriver
+       DrinkTimer(0,0,50000,0,0,0,0,15000);
       break;
       case 5 :
         // Margarita
-       DrinkTimer(0,0,0,50000,0,0,0,150000);
+       DrinkTimer(0,0,0,50000,0,0,0,15000);
       break;
       case 6 :
         // Gin & Juice
-       DrinkTimer(0,50000,0,0,0,150000,0,0);
+       DrinkTimer(0,50000,0,0,0,15000,0,0);
       break;
       case 7 :
       // Tequila Sunrise
-       DrinkTimer(0,0,0,50000,0,0,0,150000);
+       DrinkTimer(0,0,0,50000,0,0,0,15000);
       break;
       case ((sizeof(ComboArray))/2)+1:
       selection = 1;
       break;
   }
+
 }
 void DrinkTimer (long T1,long T2,long T3,long T4,long T5,long T6,long T7,long T8) {
   unsigned long timer = millis();
-  Serial.println(timer);
-    if (T1 != 0) 
-      digitalWrite(R1,LOW);
-      if (timer >= T1)
-        digitalWrite(R1,HIGH);
 
-     if (T2 != 0) 
-                  digitalWrite(R2,LOW);
-      if (timer >= T2)
-        digitalWrite(R2,HIGH);  
-
-            if (T3 != 0) 
-                  digitalWrite(R3,LOW);
-      if (timer >= T3)
-        digitalWrite(R3,HIGH);  
-
-            if (T4 != 0) 
-                  digitalWrite(R4,LOW);
-      if (timer >= T4)
-        digitalWrite(R4,HIGH);  
-
-            if (T5 != 0) 
-                  digitalWrite(R5,LOW);
-      if (timer >= T5)
-        digitalWrite(R5,HIGH);  
-
-            if (T6 != 0) 
-                  digitalWrite(R6,LOW);
-      if (timer >= T6)
-        digitalWrite(R6,HIGH);  
-
-            if (T7 != 0) 
-                  digitalWrite(R7,LOW);
-      if (timer >= T7)
-        digitalWrite(R7,HIGH);  
-
-            if (T8 != 0) 
-                  digitalWrite(R8,LOW);
-      if (timer >= T8)
-        digitalWrite(R8,HIGH);         
+Serial.println(timer - previousTimer);
+if (T1 != 0) 
+digitalWrite(R1,LOW);
+if ((unsigned long)(timer - previousTimer) >= T1){
+digitalWrite(R1,HIGH); 
+T1 = 0 ;}
+if (T2 != 0) 
+digitalWrite(R2,LOW);
+if ((unsigned long)(timer - previousTimer) >= T2){
+digitalWrite(R2,HIGH);  
+T2 = 0;}
+if (T3 != 0) 
+digitalWrite(R3,LOW);
+if ((unsigned long)(timer - previousTimer) >= T3){
+digitalWrite(R3,HIGH);  
+T3 = 0;}
+if (T4 != 0) 
+digitalWrite(R4,LOW);
+if ((unsigned long)(timer - previousTimer) >= T4){
+digitalWrite(R4,HIGH);  
+T4 = 0;}
+if (T5 != 0) 
+digitalWrite(R5,LOW);
+if ((unsigned long)(timer - previousTimer) >= T5){
+digitalWrite(R5,HIGH);  
+T5 = 0;}
+if (T6 != 0) 
+digitalWrite(R6,LOW);
+if ((unsigned long)(timer - previousTimer) >= T6){
+digitalWrite(R6,HIGH);  
+T6 = 0 ;}
+if (T7 != 0) 
+digitalWrite(R7,LOW);
+if ((unsigned long)(timer - previousTimer) >= T7){
+digitalWrite(R7,HIGH);  
+T7 = 0;}
+if (T8 != 0) 
+digitalWrite(R8,LOW);
+if ((unsigned long)(timer - previousTimer) >= T8){
+digitalWrite(R8,HIGH);
+T8 = 0;}
+if ((T1+T2+T3+T4+T5+T6+T7+T8) == 0)
+{
+   Dispense = false;
+   previousTimer = timer;
+  Serial.print("PrevValue : ");
+  Serial.println(previousTimer);
+}
+  
 }
--- DispenserMain/relaytest/relaytest.ino
+++ DispenserMain/relaytest/relaytest.ino
@@ -4,8 +4,10 @@ int R1 = 2;
 void setup() {
   // put your setup code here, to run once:
 pinMode(R1,OUTPUT);
-digitalWrite(R1,LOW);
+digitalWrite(R1,HIGH);
 delay(100);
+digitalWrite(R1,LOW);
+delay(20000);
 digitalWrite(R1,HIGH);
 delay(100);
 }
